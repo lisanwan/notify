1c37636cf5f0e7b3bb36ba539d33694f849256d6
李三碗 <73498332+lisanwan@users.noreply.github.com>
2021-07-10 20:50:54 +0800

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,11 @@
 module.exports = {
     port: 3000,
     notify: {
-        ID: '换成自己的', // 企业ID
-        SECRET: '换成自己的', // 应用Secret
-        AGENTID: "换成自己的" // 应用ID
+        ID: '
+wwe7c2b478afc94a14 ', // 企业ID
+        SECRET: '
+yB9RjceN5Bl7zRMcC19YLkgt9uo-8P9bYpV0DAZkVNk', // 应用Secret
+        AGENTID: "
+1000002" // 应用ID
     },
 }
